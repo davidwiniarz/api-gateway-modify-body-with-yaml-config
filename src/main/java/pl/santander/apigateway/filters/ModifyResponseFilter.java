4c22982d41b593749ce8e1deabd29cfa974c7442
daw-s@wp.pl <daw-s@wp.pl>
2022-02-01 13:28:29 +0100

    Refactor

--- src/main/java/pl/santander/apigateway/filters/ModifyResponseFilter.java
+++ src/main/java/pl/santander/apigateway/filters/ModifyResponseFilter.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import groovy.util.logging.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.NettyWriteResponseFilter;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -17,6 +18,7 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ReactiveHttpOutputMessage;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
@@ -32,6 +34,7 @@ import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -45,7 +48,7 @@ public class ModifyResponseFilter extends AbstractGatewayFilterFactory<ModifyRes
     private final Map<String, MessageBodyDecoder> messageBodyDecoders;
     private final Map<String, MessageBodyEncoder> messageBodyEncoders;
 
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     public ModifyResponseFilter(Set<MessageBodyDecoder> messageBodyDecoders,
                                 Set<MessageBodyEncoder> messageBodyEncoders) {
@@ -57,124 +60,110 @@ public class ModifyResponseFilter extends AbstractGatewayFilterFactory<ModifyRes
         objectMapper = new ObjectMapper();
     }
 
-    protected String applyTransform(String input, ModifyResponseFilter.Config config) {
-        // we're not doing anything fancy here
-        return input.toUpperCase();
-    }
-
     @Override
     public GatewayFilter apply(Config config) {
-        return new OrderedGatewayFilter((exchange, chain) -> {
-            ServerHttpResponse originalResponse = exchange.getResponse();
-            ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
-                @SuppressWarnings("unchecked")
-                @Override
-                public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+        return new OrderedGatewayFilter(this::filter, NettyWriteResponseFilter.WRITE_RESPONSE_FILTER_ORDER - 1);
+    }
 
-                    Class<String> inClass = String.class;
-                    Class<JsonRoot> outClass = JsonRoot.class;
+    private Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpResponse originalResponse = exchange.getResponse();
+        ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+            @SuppressWarnings("unchecked")
+            @Override
+            public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
 
-                    String originalResponseContentType = exchange.getAttribute(ORIGINAL_RESPONSE_CONTENT_TYPE_ATTR);
-                    HttpHeaders httpHeaders = new HttpHeaders();
-                    // explicitly add it in this way instead of
-                    // 'httpHeaders.setContentType(originalResponseContentType)'
-                    // this will prevent exception in case of using non-standard media
-                    // types like "Content-Type: image"
-                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, originalResponseContentType);
-                    ClientResponse clientResponse = prepareClientResponse(body, httpHeaders);
+                Class<String> inClass = String.class;
+                Class<JsonRoot> outClass = JsonRoot.class;
 
-                    // TODO: flux or mono
-                    Mono<JsonRoot> modifiedBody = extractBody(exchange, clientResponse, inClass)
-                            .flatMap(originalBody -> {
-                                JsonRoot jsonHeadersObject = null;
-                                try {
-                                    jsonHeadersObject = objectMapper.readValue(originalBody, JsonRoot.class);
-                                    System.out.println(jsonHeadersObject);
-                                } catch (JsonProcessingException e) {
-                                    System.out.println("cannot convert to json");
-                                }
-                                if(jsonHeadersObject == null)
-                                    originalBody = jsonHeadersObject.toString();
-                                System.out.println(originalBody);
-                                return Mono.just(jsonHeadersObject)
-                                        .switchIfEmpty(Mono.empty());
-                            });
+                String originalResponseContentType = exchange.getAttribute(ORIGINAL_RESPONSE_CONTENT_TYPE_ATTR);
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.add(HttpHeaders.CONTENT_TYPE, originalResponseContentType);
+                ClientResponse clientResponse = prepareClientResponse(body, httpHeaders);
 
-                    BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, outClass);
-                    CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange,
-                            exchange.getResponse().getHeaders());
-                    return bodyInserter.insert(outputMessage, new BodyInserterContext()).then(Mono.defer(() -> {
-                        Mono<DataBuffer> messageBody = writeBody(getDelegate(), outputMessage, outClass);
-                        HttpHeaders headers = getDelegate().getHeaders();
-                        if (!headers.containsKey(HttpHeaders.TRANSFER_ENCODING)
-                            || headers.containsKey(HttpHeaders.CONTENT_LENGTH)) {
-                            messageBody = messageBody.doOnNext(data -> headers.setContentLength(data.readableByteCount()));
-                        }
-                        // TODO: fail if isStreamingMediaType?
-                        return getDelegate().writeWith(messageBody);
-                    }));
-                }
+                Mono<JsonRoot> modifiedBody = extractBody(exchange, clientResponse, inClass)
+                        .flatMap(originalBody -> {
+                            JsonRoot jsonHeadersObject = null;
+                            try {
+                                jsonHeadersObject = objectMapper.readValue(originalBody, JsonRoot.class);
+                            } catch (JsonProcessingException e) {
+                                System.out.println("cannot convert to json");
+                            }
+                            assert jsonHeadersObject != null;
+                            return Mono.just(jsonHeadersObject)
+                                    .switchIfEmpty(Mono.empty());
+                        });
 
-                @Override
-                public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
-                    return writeWith(Flux.from(body).flatMapSequential(p -> p));
-                }
-
-                private ClientResponse prepareClientResponse(Publisher<? extends DataBuffer> body, HttpHeaders httpHeaders) {
-                    ClientResponse.Builder builder;
-                    builder = ClientResponse.create(exchange.getResponse().getStatusCode(), HandlerStrategies.withDefaults().messageReaders());
-                    return builder.headers(headers -> headers.putAll(httpHeaders)).body(Flux.from(body)).build();
-                }
-
-                private <T> Mono<T> extractBody(ServerWebExchange exchange, ClientResponse clientResponse, Class<T> inClass) {
-                    // if inClass is byte[] then just return body, otherwise check if
-                    // decoding required
-                    if (byte[].class.isAssignableFrom(inClass)) {
-                        return clientResponse.bodyToMono(inClass);
+                BodyInserter<Mono<JsonRoot>, ReactiveHttpOutputMessage> bodyInserter = BodyInserters.fromPublisher(modifiedBody, outClass);
+                CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange,
+                        exchange.getResponse().getHeaders());
+                return bodyInserter.insert(outputMessage, new BodyInserterContext()).then(Mono.defer(() -> {
+                    Mono<DataBuffer> messageBody = writeBody(getDelegate(), outputMessage, outClass);
+                    HttpHeaders headers = getDelegate().getHeaders();
+                    if (!headers.containsKey(HttpHeaders.TRANSFER_ENCODING)
+                        || headers.containsKey(HttpHeaders.CONTENT_LENGTH)) {
+                        messageBody = messageBody.doOnNext(data -> headers.setContentLength(data.readableByteCount()));
                     }
+                    return getDelegate().writeWith(messageBody);
+                }));
+            }
 
-                    List<String> encodingHeaders = exchange.getResponse().getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
-                    for (String encoding : encodingHeaders) {
-                        MessageBodyDecoder decoder = messageBodyDecoders.get(encoding);
-                        if (decoder != null) {
-                            return clientResponse.bodyToMono(byte[].class).publishOn(Schedulers.parallel()).map(decoder::decode)
-                                    .map(bytes -> exchange.getResponse().bufferFactory().wrap(bytes))
-                                    .map(buffer -> prepareClientResponse(Mono.just(buffer),
-                                            exchange.getResponse().getHeaders()))
-                                    .flatMap(response -> response.bodyToMono(inClass));
-                        }
-                    }
+            @Override
+            public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
+                return writeWith(Flux.from(body).flatMapSequential(p -> p));
+            }
 
+            private ClientResponse prepareClientResponse(Publisher<? extends DataBuffer> body, HttpHeaders httpHeaders) {
+                ClientResponse.Builder builder;
+                builder = ClientResponse.create(Objects.requireNonNull(exchange.getResponse().getStatusCode()), HandlerStrategies.withDefaults().messageReaders());
+                return builder.headers(headers -> headers.putAll(httpHeaders)).body(Flux.from(body)).build();
+            }
+
+            private <T> Mono<T> extractBody(ServerWebExchange exchange, ClientResponse clientResponse, Class<T> inClass) {
+                if (byte[].class.isAssignableFrom(inClass)) {
                     return clientResponse.bodyToMono(inClass);
                 }
 
-                private Mono<DataBuffer> writeBody(ServerHttpResponse httpResponse, CachedBodyOutputMessage message,
-                                                   Class<?> outClass) {
-                    Mono<DataBuffer> response = DataBufferUtils.join(message.getBody());
-                    if (byte[].class.isAssignableFrom(outClass)) {
-                        return response;
+                List<String> encodingHeaders = exchange.getResponse().getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
+                for (String encoding : encodingHeaders) {
+                    MessageBodyDecoder decoder = messageBodyDecoders.get(encoding);
+                    if (decoder != null) {
+                        return clientResponse.bodyToMono(byte[].class).publishOn(Schedulers.parallel()).map(decoder::decode)
+                                .map(bytes -> exchange.getResponse().bufferFactory().wrap(bytes))
+                                .map(buffer -> prepareClientResponse(Mono.just(buffer),
+                                        exchange.getResponse().getHeaders()))
+                                .flatMap(response -> response.bodyToMono(inClass));
                     }
+                }
 
-                    List<String> encodingHeaders = httpResponse.getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
-                    for (String encoding : encodingHeaders) {
-                        MessageBodyEncoder encoder = messageBodyEncoders.get(encoding);
-                        if (encoder != null) {
-                            DataBufferFactory dataBufferFactory = httpResponse.bufferFactory();
-                            response = response.publishOn(Schedulers.parallel()).map(buffer -> {
-                                byte[] encodedResponse = encoder.encode(buffer);
-                                DataBufferUtils.release(buffer);
-                                return encodedResponse;
-                            }).map(dataBufferFactory::wrap);
-                            break;
-                        }
-                    }
+                return clientResponse.bodyToMono(inClass);
+            }
 
+            private Mono<DataBuffer> writeBody(ServerHttpResponse httpResponse, CachedBodyOutputMessage message,
+                                               Class<?> outClass) {
+                Mono<DataBuffer> response = DataBufferUtils.join(message.getBody());
+                if (byte[].class.isAssignableFrom(outClass)) {
                     return response;
                 }
 
-            };
-            return chain.filter(exchange.mutate().response(decoratedResponse).build());
-        }, NettyWriteResponseFilter.WRITE_RESPONSE_FILTER_ORDER - 1);
+                List<String> encodingHeaders = httpResponse.getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
+                for (String encoding : encodingHeaders) {
+                    MessageBodyEncoder encoder = messageBodyEncoders.get(encoding);
+                    if (encoder != null) {
+                        DataBufferFactory dataBufferFactory = httpResponse.bufferFactory();
+                        response = response.publishOn(Schedulers.parallel()).map(buffer -> {
+                            byte[] encodedResponse = encoder.encode(buffer);
+                            DataBufferUtils.release(buffer);
+                            return encodedResponse;
+                        }).map(dataBufferFactory::wrap);
+                        break;
+                    }
+                }
+
+                return response;
+            }
+
+        };
+        return chain.filter(exchange.mutate().response(decoratedResponse).build());
     }
 
 
